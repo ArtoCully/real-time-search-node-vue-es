9384cca7b5425df80c705951aad671de7bff6b1f
Arturo Cullinane <artocully@users.noreply.github.com>
2021-08-17 16:52:06 +0100

    All origin cors

--- server/index.js
+++ server/index.js
@@ -10,33 +10,33 @@ const { elasticUrl } = require('./elasticsearch');
 
 function start() {
 
-  const whitelist = [elasticUrl];
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
+  // const whitelist = [elasticUrl];
+  // const corsOptions = {
+  //   origin: function (origin, callback) {
+  //     if (whitelist.indexOf(origin) !== -1) {
+  //       callback(null, true)
+  //     } else {
+  //       callback(new Error('Not allowed by CORS'))
+  //     }
+  //   }
+  // }
 
-  return app.use(cors(corsOptions))
+  return app.use(cors())
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
     .use("/v1", routes)
     .use((_req, res) => res.status(404).json({ success: false,error: "Route not found" }))
     .use(express.static(path.join(__dirname, "../client/public")))
-    // // enable CORS
-    // .use(function (req, res, next) {
-    //     res.header("Access-Control-Allow-Origin", "*");
-    //     res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    //     res.header(
-    //       "Access-Control-Allow-Headers",
-    //       "Origin, X-Requested-With, Content-Type, Accept"
-    //     );
-    //     next();
-    // })
+    // enable CORS
+    .use(function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    })
     .listen(port, () => {
       console.log("Express server listening on port " + port);
       console.log("Elastic server url " + elasticUrl);
