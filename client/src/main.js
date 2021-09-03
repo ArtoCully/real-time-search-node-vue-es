12333b0dcf8ad92cc01baf5479f5a2cd623ef71f
Arturo Cullinane <artocully@users.noreply.github.com>
2021-09-03 13:18:03 +0100

    Add initial vuex store

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
+import store from './store'
 import router from './router'
 import App from './App.vue'
 import './index.css';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app
+  .use(router)
+  .use(store)
+  .mount('#app');
---
+++ client/src/store/mutations.js
@@ -0,0 +1,7 @@
+import types from './mutation-types';
+
+export default {
+  [types.FETCH_CITIES](state, payload) {
+    // TODO: add functionality here
+  }
+}
