caef91f08147c6c538a425d996313f42dc4bbdd8
Arturo Cullinane <artocully@users.noreply.github.com>
2021-09-06 17:08:16 +0100

    Check for jest tests on all *.(test|spec).

--- client/jest.config.js
+++ client/jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  testMatch: [
+    '**/tests/**/**/*.spec.[jt]s?(x)',
+    '**/?(*.)+(spec|test).(tj)s?(x)'
+  ],
+  testPathIgnorePatterns: ['/node_modules/'],
 }
